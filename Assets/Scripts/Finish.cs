9b717753eb53d0ca4680c501fd9aeee34484adee
chrisRV15 <christopher.d.thomasvillalobos@lions.enc.edu>
2021-10-04 17:38:45 -0400

    100% complete
    but with a few bugs

---
+++ Assets/Scripts/Finish.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Finish : MonoBehaviour
+{
+    public GameObject creditsPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            creditsPanel.SetActive(true);
+           
+        }
+    }
+
+    public void Quit()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
